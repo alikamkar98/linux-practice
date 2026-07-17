2f21717eae414deb0cfe73499a42c7ecf299dfff
Ali Monshizadeh <alikamkar98@gmail.com>
2026-07-17 19:17:19 +0200

    add file creation demo

---
+++ 01-file-creation/demo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "== Creating an Empty file =="
+echo "hello from echo" > example.txt
+cat example.txt
+
+echo "== Appending a second line haha =="
+echo "second line" >> example.txt
+cat example.txt
